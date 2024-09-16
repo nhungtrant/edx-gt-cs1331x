c470bd5704cffd407152df1b41be1e34c574ec9b
Amelia Tran <150972227+nhungtrant@users.noreply.github.com>
2024-09-15 23:00:06 -0700

    Update amelia_Battleship.java

--- hw03/amelia_Battleship.java
+++ hw03/amelia_Battleship.java
@@ -11,20 +11,40 @@ class HelloWorld {
         int[][] player1 = new int[5][2];
         for (int i = 0; i < 5; i++ ) {
             System.out.println("Enter ship "+ (i+1)+ " location:");
-            if (input.hasNextInt()) {
-                player1[i][0] = input.nextInt();
-                player1[i][1] = input.nextInt();
-            }
+            int row = checkValidInput(input);
+            int col = checkValidInput(input);
+            checkDuplicate(row,col,player1);
+            player1[i][0] = row;
+            player1[i][1] = col;
         }
         char[][] player1GridMap = new char[5][5];
         for (int[] row : player1)
             System.out.println(Arrays.toString(row));
-
     }
     private static void printInvalidInputMessage() {
         System.out.println("Invalid input entered. Terminating...");
     }
     
+    private static int checkValidInput(Scanner input) {
+        if (input.hasNextInt()) {
+            int num = input.nextInt();
+            if (num>0 && num<6) {
+                return num;
+            }
+        }
+        input.nextLine();
+        System.out.println("Invalid coordinates. Choose different coordinates.");
+        return -1;
+    }
+    
+    private static void checkDuplicate(int row, int col, int[][] matrix_ships) {
+        for (int[] ship: matrix_ships){
+            if ((row == ship[0]) && (col == ship[1])){
+                System.out.println("Match ship");
+            }
+        }
+    }
+
     // Use this method to print game boards to the console.
 	private static void printBattleShip(int[][] player) {
 		System.out.print("  ");
