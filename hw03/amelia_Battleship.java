e8a057c55990d07bf36b65e0669eec8a31d27644
Amelia Tran <150972227+nhungtrant@users.noreply.github.com>
2024-09-24 14:50:57 -0700

    Update amelia_Battleship.java

--- hw03/amelia_Battleship.java
+++ hw03/amelia_Battleship.java
@@ -1,41 +1,50 @@
 // Online Java Compiler
 // Use this editor to write, compile and run your Java code online
 import java.util.Scanner;
-import java.util.Arrays; //remove?
 
 class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Welcome to Battleship!");
         System.out.println("PLAYER 1, ENTER YOUR SHIPS' COORDINATES.");
         Scanner input = new Scanner(System.in); //Initialize scanner object
-        int[][] player1 = new int[5][2];
+        
+        int[][] player1_Position = ShipPositionCollector(input);
+        char[][] player1GridMap = ShipGridMap(player1_Position);
+        printBattleShip(player1GridMap);
+        
+        char[][] player2GridMap = new char[5][5];
+    }
+    
+    private static int[][] ShipPositionCollector(Scanner input) {
+        int[][] player = new int[5][2];
         for (int i = 0; i < 5; i++ ) {
             boolean check = false;
             while (!check) {
                 System.out.println("Enter ship "+ (i+1)+ " location:");
                 int row = checkValidInput(input);
                 int col = checkValidInput(input);
-                check = checkDuplicate(row,col,player1);
-                player1[i][0] = row;
-                player1[i][1] = col;
+                check = checkDuplicate(row,col,player);
+                player[i][0] = row - 1;
+                player[i][1] = col - 1;
             }
         }
-        char[][] player1GridMap = new char[5][5];
+        return player;
+    }
+    
+    private static char[][] ShipGridMap(int[][] ShipPosition) {
+        char[][] playerGridMap = new char[5][5];
         
-        for (char[] map : player1GridMap) {
+        for (char[] map : playerGridMap) {
             for (char item : map) {
                 item = '-';
             }
         }
-        for (int[] ship : player1) {
-            // System.out.println(Arrays.toString(line));
-            player1GridMap[ship[0]][ship[1]] = '@';
+        for (int[] ship : ShipPosition) {
+            playerGridMap[ship[0]][ship[1]] = '@';
         }
-        
-        printBattleShip(player1GridMap);
-        
-        char[][] player1GridMap = new char[5][5];
+        return playerGridMap;
     }
+    
     private static void printInvalidInputMessage() {
         System.out.println("Invalid input entered. Terminating...");
     }
@@ -43,13 +52,12 @@ class HelloWorld {
     private static int checkValidInput(Scanner input) {
         if (input.hasNextInt()) {
             int num = input.nextInt();
-            if (num>-1 && num<5) {
+            if (num>0 && num<6) {
                 return num;
             }
         }
-        // input.nextLine();
-        System.out.println("Invalid coordinates. Choose different coordinates.");
         input.nextLine();
+        System.out.println("Invalid coordinates. Choose different coordinates.");
         return -1;
     }
     
