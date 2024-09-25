db76607b080751f1d540b87ed2be38bf5fd5ab36
Amelia Tran <150972227+nhungtrant@users.noreply.github.com>
2024-09-25 15:36:07 -0700

    Update amelia_Battleship.java

--- hw03/amelia_Battleship.java
+++ hw03/amelia_Battleship.java
@@ -12,7 +12,31 @@ class HelloWorld {
         char[][] player1GridMap = ShipGridMap(player1_Position);
         printBattleShip(player1GridMap);
         
+        
+        boolean destroy = false;
+        while (!destroy) {
+            int shipDestroyed = 0;
+            // funtion to take in input and check
+            if (shipDestroyed = 5) {
+                destroy = true;
+            }
+        }
+        
         char[][] player2GridMap = new char[5][5];
+    }
+    
+    private static int[][] TargetCollector(Scanner input, int[][] shipPosition) {
+        int[][] player = new int[25][2];
+        boolean check = false;
+        while (!check) {
+            System.out.println("Player 1, enter hit row/column:");
+            int row = checkValidInput(input);
+            int col = checkValidInput(input);
+            check = checkDuplicate(row,col,player);
+            player[i][0] = row - 1;
+            player[i][1] = col - 1;
+        }
+        return player;
     }
     
     private static int[][] ShipPositionCollector(Scanner input) {
